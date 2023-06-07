be3c91bf20f77558868be44548c0ebca20630561
Ayu Nakata <mieze018@users.noreply.github.com>
2023-06-08 01:15:45 +0900

    7.3.3 バリデーションとエラーメッセージの表示

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,5 @@
 class Article < ApplicationRecord
+  # presence: true はrequiredのこと　出席とか出頭みたいな意味　くせつよ
+  validates :title, presence: true
+  validates :body, presence: true, length: { minimum: 10 }
 end
