10f90a33208ddf339fdefbf2a32d445e6c3fb838
Ayu Nakata <mieze018@users.noreply.github.com>
2023-06-08 03:02:15 +0900

    8.2 モデル同士を関連付ける

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ApplicationRecord
+  has_many :comments
   # presence: true はrequiredのこと　出席とか出頭みたいな意味　くせつよ
   validates :title, presence: true
   validates :body, presence: true, length: { minimum: 10 }
