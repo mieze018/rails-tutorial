363c8b52e8b6dc851cd756f13f4a44b9ab834bf5
Ayu Nakata <mieze018@users.noreply.github.com>
2023-06-07 22:58:30 +0900

    4.2 Railsで「Hello」と表示する
    ルーティングを追加して以下を実行
    $ bin/rails generate controller Articles index --skip-routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  # GET /articlesリクエストをArticlesControllerのindexアクションに対応付ける
+  get 'articles', to: 'articles#index'
+  # 以下のコマンドを実行してコントローラーを自動作成する
+  # $ bin/rails generate controller Articles index --skip-routes
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
