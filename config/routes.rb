176ccce469a6f3fd01b333d665226eda892ca8dc
Ayu Nakata <mieze018@users.noreply.github.com>
2023-06-08 00:17:09 +0900

    7.2 リソースフルルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,19 @@
 Rails.application.routes.draw do
   root "articles#index"
   # GET /articlesリクエストをArticlesControllerのindexアクションに対応付ける
-  get 'articles', to: 'articles#index'
+  # get 'articles', to: 'articles#index'
   # 以下のコマンドを実行してコントローラーを自動作成する
   # $ bin/rails generate controller Articles index --skip-routes
 
+  # CRUDの各アクションに対応するルーティングを一括で設定する
+  resources :articles
+
   # `:`で始まる部分は「ワイルドカード」で、その部分に入る値はコントローラーのアクション内でparams[:id]として参照できる
   get 'articles/:id', to: 'articles#show'
+
+
+
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
