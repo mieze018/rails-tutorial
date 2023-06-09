5a8ff97992ae5474fe5028bc2f35ad197c92ceb8
Ayu Nakata <mieze018@users.noreply.github.com>
2023-06-09 16:59:34 +0900

    10.1 関連付けられたオブジェクトも削除する

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,6 @@
 class Article < ApplicationRecord
   include Visible
-  has_many :comments
+  has_many :comments, dependent: :destroy
   # presence: true はrequiredのこと　出席とか出頭みたいな意味　くせつよ
   validates :title, presence: true
   validates :body, presence: true, length: { minimum: 10 }
